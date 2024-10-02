4c565cf2e4944a948b3b8cb1aede683b34f1ce95
nishantaMishra <nishunm2000@gmail.com>
2024-10-02 19:15:41 +0530

    update

--- data-visualise/draw_pair_plot.py
+++ data-visualise/draw_pair_plot.py
@@ -12,25 +12,20 @@ def draw_pair_plot(df, columns):
         diag_kind = input("Enter diag_kind (e.g., 'auto', 'hist', 'kde'): ").strip()
         kind = input("Enter kind (e.g., 'scatter', 'reg', 'hex'): ").strip()
 
-        # Set default values if user presses enter without providing values
         diag_kind = diag_kind if diag_kind else 'auto'
         kind = kind if kind else 'scatter'
 
-        # Convert column names to numbers
         column_numbers = [col_alpha_to_num(col) if not col.isnumeric() else int(col) for col in columns]
 
-        # Check if columns are within bounds
         if any(col < 0 or col >= df.shape[1] for col in column_numbers):
             print(f"{colored('Caution!','yellow' )} One or more columns are out of range.")
             return
 
-        # Check if columns are found in the DataFrame
         invalid_columns = [col for col in column_numbers if col not in range(df.shape[1])]
         if invalid_columns:
             print(f"{colored('Caution!','yellow' )} Column(s) {invalid_columns} not found in the CSV file.")
             return
 
-        # Create pair plot
         sns.pairplot(df.iloc[:, column_numbers], diag_kind=diag_kind, kind=kind)
         plt.show()
 
